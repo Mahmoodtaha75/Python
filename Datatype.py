13a5b0fee230571c930502af49f93c5add1be9a7
Mahmoodtaha75 <152767736+Mahmoodtaha75@users.noreply.github.com>
2024-09-01 22:25:49 +0530

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,48 @@
+#integer data type
+a = 10
+print("type of a:",type(a))
+print("value of a:",a)
+
+#float data type
+b = 10.5
+print("type of b:",type(b))
+print("value of b:",b)
+
+#string data type
+c = "python"
+print("type of c:",type(c))
+print("value of c:",c)
+
+#boolean data type
+d = True
+print("value of d",d)
+print("type of d",type(d))
+
+#list data type
+e = [1,2,3,4]
+print("type of e:",type(e))
+print("value of e:",e)
+
+#tuple data type
+f = (9,8,7,6)
+print("type of f",type(f))
+print("values of f",f)
+
+#set data type
+
+g = {1,2,3,4}
+print("value of g:",g)
+print("type of g:",type(g))
+
+#Dictionary data type
+h = {
+    "name":"d87",
+    "age":22
+}
+print("type of h:",type(h))
+print("values of h:",type(h))
+
+#none data type
+i = None
+print("value of i:",i)
+print("type of i:",type(i))
